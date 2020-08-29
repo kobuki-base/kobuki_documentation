317bfa9524d9cccb843cbfee4ea3af17dd8d1e97
Daniel Stonier <d.stonier@gmail.com>
2020-08-29 13:34:32 -0400

    [software] versioning advice

--- doc/conf.py
+++ doc/conf.py
@@ -16,7 +16,13 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-# sys.path.insert(0, os.path.abspath('.'))
+##############################################################################
+# Preparation
+##############################################################################
+
+import os
+
+on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
 
 ##############################################################################
 # Todo
